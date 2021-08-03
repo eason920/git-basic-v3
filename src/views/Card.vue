60c02f34afcb6aa353e490845e91408c20fcac93
eason920 <eason920@gmail.com>
2021-08-04 00:40:28 +0800

    start

---
+++ src/views/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>IS CARD PAGE</h1>
+    <Card title="Basic Exercise"/>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/git-basic-v3/dist/'
+}
